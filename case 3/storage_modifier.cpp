65c29f614399d251f2a75f4b465d382f664f7d64
prasunaarumalla15 <53810285+prasunaarumalla15@users.noreply.github.com>
2020-04-16 16:59:15 +0530

    Add files via upload

--- case 3/storage_modifier.cpp
+++ case 3/storage_modifier.cpp
@@ -8,9 +8,12 @@
 #include<string.h>
 using namespace std;
 
-unsigned iNum=96;    //unsigned global variable initially holds 96
-signed int iNum1=100; //signed int initially holds 100
-int iX; //initialized to '0'
+//unsigned global variable initially holds 96
+unsigned iNum=96;
+//signed int initially holds 100
+signed int iNum1=100;
+//extern variable initialized to 0
+int iExtern_Num2;
 
 /*Function Name :test_function_storage
   Parameters    :no parameter
@@ -19,11 +22,12 @@ int iX; //initialized to '0'
 
 void test_function_storage()
 {
-    long double fCount;         //long double modifier
+    //long double modifier
+    long double fCount;
     cout<<"Enter the double value:";
     cin>>fCount;
-    static int iStatic_Number=5;    //static variable, initially holds 0,it initialize only once
-    register int iRegister_Number;    //register variable, always starts freshly
+     //register variable, always starts freshly,stored in cpu registers,very fast
+    register int iRegister_Number;
     cout<<"Enter the Register variable:";
     cin>>iRegister_Number;
     cout<<endl;
@@ -68,7 +72,8 @@ void nonStaticFun()
   Return Type   :auto return type
   Usage         :to know the usage of auto keyword*/
 
-auto fun()      //To understand how auto takes automatic return type based on compiler
+//To understand how auto takes automatic return type based on compiler
+auto fun()
 {
     cout<<"Welcome to HCL"<<endl;
 }
@@ -81,7 +86,8 @@ auto fun()      //To understand how auto takes automatic return type based on co
 
 void type_modifiers()
 {
-    unsigned iNum;  //if it was not given with correct sign it gives garbage value
+    //if it was not given with correct sign it gives garbage value
+    unsigned iNum;
     cout<<"Enter a unsigned Number:";
     cin>>iNum;
     signed int iNum1;
@@ -92,19 +98,22 @@ void type_modifiers()
     cout<<"short int:"<<iNum2<<'\t'<<"size of short int:"<<sizeof(iNum2)<<endl;
     cout<<endl;
     cout<<"Local variables"<<endl;
-    cout<<"Global Variable in function = "<<iNum<<" "<<"Size:"<<sizeof(iNum)<<endl;//size is always same for integer variables
+    //size is always same for integer variables
+    cout<<"Global Variable in function = "<<iNum<<" "<<"Size:"<<sizeof(iNum)<<endl;
     cout<<"Second Global Variable in function = "<<iNum1<<" "<<"Size:"<<sizeof(iNum1)<<endl;
     cout<<"global variable outside the function when having inside function also"<<endl;
-    cout<<::iNum<<'\t'<<::iNum1<<endl;  //displaying global variables
+     //displaying global variables use scope resolution
+    cout<<::iNum<<'\t'<<::iNum1<<endl;
     cout<<endl;
 }
 
 int main(int argc,char* argv[])
 {
-    extern int iX;
-    if(argc>=2)
+    extern int iExtern_Num2;
+    if(argc==2)
     {
-        if(strcmp(argv[1],"-h")==0)     //created a help command
+        //created a help command
+        if(strcmp(argv[1],"-h")==0)
         {
             cout<<"Input given inputs"<<endl;
         }
@@ -114,14 +123,14 @@ int main(int argc,char* argv[])
         fun();
         staticFun();
         nonStaticFun();
-        cout<<"value of the variable which is globally declared:"<<iX<<endl;
+        cout<<"value of the variable which is globally declared can be considered as extern also:"<<iExtern_Num2<<endl;
         test_function_storage();
         type_modifiers();
         staticFun();
         nonStaticFun();
         cout<<"change the value of extern variable:";
-        cin>>iX;
-        cout<<"value of the extern variable after changing:"<<iX<<endl;
+        cin>>iExtern_Num2;
+        cout<<"value of the extern variable after changing:"<<iExtern_Num2<<endl;
         staticFun();
         nonStaticFun();
     }
